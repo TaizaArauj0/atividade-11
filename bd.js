1388e620783451320a1813406cd423e590cbb559
Tay Araujo <132668816+TaizaArauj0@users.noreply.github.com>
2023-08-11 14:29:27 +0000

    araujo

--- bd.js
+++ bd.js
@@ -1,6 +1,6 @@
 //bd.js
 import pkg from "pg";
-export { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario  };
+  export { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario, updateUsuario };
 const { Pool } = pkg;
 
 async function connect() {
@@ -16,7 +16,6 @@ async function selectUsuarios(id) {
   return res.rows;
 }
 
-//bd.js
 async function selectUsuario(id) {
   const client = await connect();
   const query = "SELECT * FROM usuario WHERE id = $1";
@@ -38,3 +37,11 @@ async function deleteUsuario(id) {
   await client.query(query, [id]);
 }
 
+async function updateUsuario(data) {
+  const client = await connect();
+  const query =
+    "UPDATE usuario SET nome = $1, email = $2, senha = $3 WHERE id = $4";
+  const usuario = [data.nome, data.email, data.senha, data.id];
+  await client.query(query, usuario);
+
+}
